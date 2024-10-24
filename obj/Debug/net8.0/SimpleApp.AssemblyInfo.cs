52c1c2f72640cd98c5c053795be8565fac71a973
Script-Eng <lesalonsteven@gmail.com>
2024-10-24 23:31:39 +0200

    Nav links modification

--- obj/Debug/net8.0/SimpleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b264fbf5a02a7fdadb07e0fe54f01eab3edc86c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011414ea46edcc9f1f75e57756ace4371b81b218")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
